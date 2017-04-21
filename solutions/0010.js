0d983eff24e74a8b49cc1750432a522569fcf2c3
Markus Tran <thebluepillow@gmail.com>
2017-04-21 16:41:47 -0700

    sol 0010

--- solutions/0010.js
+++ solutions/0010.js
@@ -1,13 +1,16 @@
 const oddPrimes = [];
+let sum = 2;
 
-search: for (let next = 3; oddPrimes.length < 10000; next += 2) {
+search: for (let next = 3; next < 2000000; next += 2) {
   for (const prime of oddPrimes) {
     if (next % prime === 0) {
       continue search;
     }
   }
 
+  sum += next;
   oddPrimes.push(next);
+  console.log(next);
 }
 
-module.exports = oddPrimes[10000 - 1];
+module.exports = sum;
