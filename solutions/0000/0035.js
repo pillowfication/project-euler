32f954f75e4bca6e0d652539c1a96b729bbb7ab7
Markus Tran <thebluepillow@gmail.com>
2017-05-10 12:06:22 -0700

    sol 0035

---
+++ solutions/0000/0035.js
@@ -0,0 +1,41 @@
+/**
+ * Circular primes
+ *
+ * The number, 197, is called a circular prime because all rotations of the
+ * digits: 197, 971, and 719, are themselves prime.
+ *
+ * There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37,
+ * 71, 73, 79, and 97.
+ *
+ * How many circular primes are there below one million?
+ */
+
+module.exports = () => {
+  const isNotPrime = [];
+  const primes = {};
+
+  for (let num = 2; num < 2000000; ++num) {
+    if (isNotPrime[num]) {
+      continue;
+    }
+
+    primes[num] = true;
+
+    for (let notPrime = num * 2; notPrime < 2000000; notPrime += num) {
+      isNotPrime[notPrime] = true;
+    }
+  }
+
+  let countPrimes = 0;
+
+  search: for (const prime in primes) {
+    for (let i = 0; i < prime.length; ++i) {
+      if (!primes[prime.slice(i) + prime.slice(0, i)]) {
+        continue search;
+      }
+    }
+    ++countPrimes;
+  }
+
+  return countPrimes;
+};
